b3e137106da25d8e1fbda68161e996fa8c8b6430
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-06-21 23:49:49 +0530

    Add project files.

---
+++ Dynmic binding ya Run time polymorphism/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Dynmic_binding_ya_Run_time_polymorphism
+{
+
+    class MyClass
+    {
+        public  virtual void show()
+        {
+            Console.WriteLine("Hello :");
+        }
+    }
+
+
+    class Program : MyClass
+    {
+        public override void show()
+        {
+            Console.WriteLine("Hii :");
+        }
+        static void Main(string[] args)
+        {
+         MyClass obj = new Program();
+            obj.show();
+        }
+    }
+}
